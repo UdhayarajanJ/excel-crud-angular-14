c81f4cd7ef854eaf81bd3682370e8fa6f07575cd
Udhayarajan J <udhayarajan080@gmail.com>
2022-11-10 18:05:24 +0530

    Reactive Form Modules And Validation Functionalities Implementation Succeed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { LoggerService } from './core/services/logger.service';
@@ -8,7 +10,8 @@ import { LoggerService } from './core/services/logger.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit
+{
 
   //Declare Template Reference Variable
   @ViewChild('openModalPopup') openModalPopup!: ElementRef;
@@ -19,32 +22,73 @@ export class AppComponent implements OnInit {
   @ViewChild('optionCheckedCreateFile') optionCheckedCreateFile!: ElementRef;
   @ViewChild('optionCheckedUploadFile') optionCheckedUploadFile!: ElementRef;
 
+  createFileFormGroup!: FormGroup;
+  isCreateFileFormIsSubmitted: boolean = false;
   constructor(
     private spinner: NgxSpinnerService,
     private logger: LoggerService,
-    private toaster: ToastrService
-  ) {
+    private toaster: ToastrService,
+    private fb: FormBuilder
+  )
+  {
 
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.logger.logInformation('CheckLogger', 'Angular 14 Excel Crud');
+    this.defaultLoadForm();
     //this.toaster.success('Success','Message');
   }
 
-  optionsChecked(e: any) {
-    if (e.target.value == 1) {
+  //Option Changes Then Shown Modal Popup
+  optionsChecked(e: any)
+  {
+    if (e.target.value == 1)
+    {
       this.openModalPopup.nativeElement.setAttribute('data-bs-target', '#btn_Download_File_Popup');
       this.openModalPopup.nativeElement.click();
     }
-    else {
+    else
+    {
       this.openModalPopup.nativeElement.setAttribute('data-bs-target', '#btn_Upload_File_Popup');
       this.openModalPopup.nativeElement.click();
     }
   }
 
-  popupCancelSelection() {
-    this.optionCheckedCreateFile.nativeElement.checked=false;
-    this.optionCheckedUploadFile.nativeElement.checked=false;
+  //Close Popup To Unchecked The Option
+  popupCancelSelection()
+  {
+    this.optionCheckedCreateFile.nativeElement.checked = false;
+    this.optionCheckedUploadFile.nativeElement.checked = false;
+  }
+
+  //Form Loading 
+  defaultLoadForm()
+  {
+    this.createFileFormGroup = this.fb.group({
+      excelTitle: ['', [Validators.required]],
+      sheetName: ['', [Validators.required]],
+      fileName: ['', [Validators.required]],
+    });
+  }
+
+  //Get Form Errors 
+  get f()
+  {
+    return this.createFileFormGroup.controls;
+  }
+
+  //Create File On Submit
+  onSubmitCreateFile()
+  {
+    this.logger.logInformation('Submit Check', 'Submit Working Fine');
+    this.isCreateFileFormIsSubmitted = true;
+    if (this.createFileFormGroup.invalid)
+      return;
+    else
+    {
+      this.logger.logInformation('Form Value', this.createFileFormGroup.value);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
+import { ExcelService } from './core/services/excel.service';
 import { LoggerService } from './core/services/logger.service';
 
 @NgModule({
@@ -11,15 +14,18 @@ import { LoggerService } from './core/services/logger.service';
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     NgxSpinnerModule,
     ToastrModule.forRoot({
       progressBar: true,
       progressAnimation: 'increasing'
-    })
+    }),
+
   ],
-  providers: [LoggerService],
+  providers: [LoggerService, ExcelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
