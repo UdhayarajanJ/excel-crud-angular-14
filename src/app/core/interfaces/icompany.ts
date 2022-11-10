7582799a8b6e04db9ec0132ac378c2b08bf64acc
Udhayarajan J <udhayarajan080@gmail.com>
2022-11-10 18:12:52 +0530

    Add New Interface File And Include Necessary Fields

---
+++ src/app/core/interfaces/icompany.ts
@@ -0,0 +1,9 @@
+export interface Icompany
+{
+    employeeName: string,
+    employeeSalary: number,
+    employeeJoininDate: string,
+    employeeRole: string,
+    employeeEmail: string,
+    employeePhone: string
+}
